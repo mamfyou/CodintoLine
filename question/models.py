9de4a1441d1b23460f7576884e4ebdf48f163770
mamf <mamf20021381@>
2022-11-30 11:55:06 +0330

    Question and question fields MODELS configured

--- question/models.py
+++ question/models.py
@@ -1,10 +1,99 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
+
+from question_sheet.models import Question
 
 
 # Create your models here.
-class QuestionParent(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+class Range(models.Model):
+    start_range_at = models.PositiveIntegerField()
+    end_range_at = models.PositiveIntegerField()
+    first_tag = models.CharField(max_length=50)
+    mid_tag = models.CharField(max_length=50)
+    end_tag = models.CharField(max_length=50)
+    has_zero = models.BooleanField(default=False)
+
+
+class Number(models.Model):
+    min_num = models.IntegerField()
+    max_num = models.IntegerField()
+    has_decimal = models.BooleanField(default=False)
+    has_negative = models.BooleanField(default=False)
+
+
+class Link(models.Model):
+    hint = models.CharField(max_length=255, null=True, blank=True)
+
+
+class Email(models.Model):
+    hint = models.CharField(max_length=255, null=True, blank=True)
+
+
+class Text(models.Model):
+    BUTTON_SHAPE = (('1', 'First'), ('2', 'Second'), ('3', 'Third'), ('4', 'Fourth'), ('5', 'Fifth'), ('6', 'Sixth'))
+    button_shape = models.CharField(max_length=20, choices=BUTTON_SHAPE, null=True, blank=True)
+    button_text = models.CharField(max_length=50, null=True, blank=True)
+
+
+class File(models.Model):
+    max_size = models.IntegerField(null=True, blank=True)
+
+
+class TextWithAnswer(models.Model):
+    validation = models.JSONField(null=True, blank=True)
+
+
+class DrawerList(models.Model):
+    options = ArrayField(models.CharField(max_length=255))
+    min_selection = models.PositiveIntegerField(null=True, blank=True)
+    max_selection = models.PositiveIntegerField(null=True, blank=True)
+    is_random_order = models.BooleanField(default=False)
+    is_alphabet_order = models.BooleanField(default=False)
+
+
+class Grading(models.Model):
+    start_grade_at = models.PositiveIntegerField()
+    end_grade_at = models.PositiveIntegerField()
+    icon = models.ImageField(upload_to='media/', null=True, blank=True)
+
+
+class GroupQuestions(models.Model):
+    BUTTON_SHAPE = (('1', 'First'), ('2', 'Second'), ('3', 'Third'), ('4', 'Fourth'), ('5', 'Fifth'), ('6', 'Sixth'))
+    button_shape = models.CharField(max_length=20, choices=BUTTON_SHAPE, null=True, blank=True)
+    button_text = models.CharField(max_length=50, null=True, blank=True)
+    is_random_order = models.BooleanField(default=False)
+    questions = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='group_questions')
+
+
+class Prioritization(models.Model):
+    options = ArrayField(models.CharField(max_length=255))
+    is_random_order = models.BooleanField(default=False)
+    is_alphabetic_order = models.BooleanField(default=False)
+
+
+class MultiChoice(models.Model):
+    options = ArrayField(models.CharField(max_length=255), models.ImageField(upload_to='media/', null=True, blank=True))
+    min_selection = models.PositiveIntegerField(null=True, blank=True)
+    max_selection = models.PositiveIntegerField(null=True, blank=True)
+    extra_actions = models.JSONField(null=True, blank=True)
+    is_random_order = models.BooleanField(default=False)
+    is_alphabetic_order = models.BooleanField(default=False)
+    is_vertical_display = models.BooleanField(default=False)
+    is_2x_picture = models.BooleanField(default=False)
+    is_select_all = models.BooleanField(default=False)
+
+
+class WelcomePage(models.Model):
+    BUTTON_SHAPE = (('1', 'First'), ('2', 'Second'), ('3', 'Third'), ('4', 'Fourth'), ('5', 'Fifth'), ('6', 'Sixth'))
+    button_shape = models.CharField(max_length=20, choices=BUTTON_SHAPE, null=True, blank=True)
+    button_text = models.CharField(max_length=50, null=True, blank=True)
+
+
+class ThanksPage(models.Model):
+    link = models.URLField(null=True, blank=True)
+    has_social_link = models.BooleanField(default=False)
+    has_eitaa = models.BooleanField(default=False)
+    has_telegram = models.BooleanField(default=False)
+    has_whatsapp = models.BooleanField(default=False)
+    has_instagram = models.BooleanField(default=False)
+    has_soroush = models.BooleanField(default=False)
--- question_sheet/models.py
+++ question_sheet/models.py
@@ -1,5 +1,27 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+
+class QuestionContentType(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': ('QuestionSheet', 'GroupQuestion')})
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class QuestionFields(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='question_fields',
+                                     limit_choices_to={'model__in': (
+                                         'Range', 'Number', 'DropDownList', 'MultiChoice', 'Link', 'Email', 'File',
+                                         'Grading', 'Prioritization',
+                                         'TextWithAnswer', 'Text', 'WelcomePage', 'ThanksPage', 'GroupQuestions')})
+
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    unique_together = ('content_type', 'object_id')
 
 
 class QuestionSheet(models.Model):
@@ -15,7 +37,6 @@ class QuestionSheet(models.Model):
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField()
     duration = models.DurationField(null=True, blank=True)
-    questions = models.ForeignKey('Question', on_delete=models.PROTECT)
     is_active = models.BooleanField(default=True)
     has_progress_bar = models.BooleanField(default=False)
     is_one_question_each_page = models.BooleanField(default=False)
@@ -26,12 +47,16 @@ class QuestionSheet(models.Model):
 
 class Question(models.Model):
     QUESTION_KINDS = (
-        'Range', 'Number', 'DropDownList', 'MultiChoice', 'Link', 'Email', 'File', 'Grading', 'Prioritization',
-        'TextWithAnswer', 'Text', 'WelcomePage', 'ThanksPage', 'GroupQuestions'
+        ('rng', 'Range'), ('num', 'Number'), ('ddlist', 'DropDownList'), ('mulch', 'MultiChoice'), ('link', 'Link'),
+        ('email', 'Email'), ('file', 'File'), ('grd', 'Grading'), ('prior', 'Prioritization'),
+        ('txtwa', 'TextWithAnswer'), ('txt', 'Text'), ('wel', 'WelcomePage'), ('thanks', 'ThanksPage'),
+        ('gq', 'GroupQuestions')
     )
     kind = models.CharField(max_length=100, choices=QUESTION_KINDS)
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500, null=True, blank=True)
     is_required = models.BooleanField(default=False)
     has_question_num = models.BooleanField(default=False)
-
+    question_parent = GenericRelation(QuestionContentType, related_query_name='question_parent')
+    question_fields = GenericRelation(QuestionFields, related_query_name='question_fields')
+    media = models.FileField(upload_to='media/', null=True, blank=True)

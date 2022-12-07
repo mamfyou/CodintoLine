8c03f2b13ee0a7ff4f6ab7165ea72adb8275d93a
mamf <mamf20021381@>
2022-12-07 10:09:21 +0330

    question sheet url configured

--- codintoLine/settings.py
+++ codintoLine/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'rest_framework',
     'generic_relations',
+    'rest_framework_nested',
 
     'question_sheet',
     'question',
--- question_sheet/urls.py
+++ question_sheet/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
-from question_sheet.views import QuestionSheetViewSet
+from question_sheet.views import *
 
 router = DefaultRouter()
 router.register('', QuestionSheetViewSet, basename='question-sheet')
+nested = NestedDefaultRouter(router, '', lookup='question_sheet')
+nested.register('preview', QuestionSheetQuestions , basename='question')
 
 urlpatterns = router.urls

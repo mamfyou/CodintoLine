d56fdebdfd0ec31ee492164cd70d05f96a7d31f2
mamf <mamf20021381@>
2022-12-07 10:04:10 +0330

    questions url configured

--- question/urls.py
+++ question/urls.py
@@ -6,5 +6,17 @@ from question.views import *
 router = DefaultRouter()
 router.register('txtwithans', TxtWithAnsViewSet, basename='text_with_answer')
 router.register('thnxpage', ThanksPageViewSet, basename='thanks-page')
+router.register('welcomepage', WelcomePageViewSet, basename='welcome-page')
+router.register('groupquestion', GroupQuestionViewSet, basename='group-question')
+router.register('multichoice', MultiChoiceViewSet, basename='multi-choice')
+router.register('prioritization', PrioritizationViewSet, basename='prioritization')
+router.register('grading', GradingViewSet, basename='grading')
+router.register('number', NumberViewSet, basename='number')
+router.register('link', LinkViewSet, basename='link')
+router.register('text', TextViewSet, basename='text')
+router.register('email', EmailViewSet, basename='email')
+router.register('file', FileViewSet, basename='file')
+router.register('drawer', DrawerListViewSet, basename='drawer')
+router.register('range', RangeViewSet, basename='range')
 
 urlpatterns = router.urls
